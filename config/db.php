6259b93b1ffd661c6df05a27b9dbeb770a5a60bd
n1ks2n <nikita@adsplay.ru>
2015-01-28 18:06:10 +0300

    Конфиг для БД

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * User: Nikita Pimoshenko <nikita.pimoshenko@yandex.ru>
+ * Date: 28.01.15
+ * Time: 17:59
+ */
+return [
+    'host' => 'localhost',
+    'dbname' => 'systemcall',
+    'user' => 'root',
+    'password' => '123',
+];
